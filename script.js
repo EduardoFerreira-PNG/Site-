3bed9605c9e60afaab5214a363648b1b4e537b6b
Eduardo <eduardonaves02@outlook.com>
2022-05-10 13:26:14 -0300

    Teste pessoal

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const btn = document.querySelector('')
+const readMorebtn = document.querySelector('.read__more--btn');
+const text = document.querySelector('.text');
 
-function funcionando() {
-  alert('você consegue meu chapa');
-}
+readMorebtn.addEventListener('click', (e)=>{
+  text.classList.toggle('show-more');
+  
+})
+
+const showMorebtn = document.querySelector('.btn__showMore');
+const answers = document.querySelector('.answers');
+
+
+  showMorebtn.addEventListener('click', (e)=>{
+  answers.classList.toggle('show__more');
+  
+})
